800d3933d59469bb6c32062b197915b2da0c8207
zxqfengdi <zxqfengdi@outlook.com>
2019-11-25 20:30:21 +0800

    add django exer code

--- python-project/books_six/books/urls.py
+++ python-project/books_six/books/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     url(r'^ip_test/$', views.ip_test, name='ip_test'),
     url(r'^show_upload/$', views.show_upload, name='show_upload'),
     url(r'^upload_handle/$', views.upload_handle, name='upload_handle'),
-    url(r'^show_areas/$', views.show_areas, name='show_areas'),
+    url(r'^show_areas(?P<pageindex>\d*)/$', views.show_areas, name='show_areas'),
+    url(r'^areas/$', views.areas, name='areas'),
+    url(r'^province/$', views.province, name='province'),
+    url(r'^city(\d+)/$', views.city, name='city'),
+    url(r'^dis(\d+)/$', views.dis, name='dis'),
 ]
--- python-project/books_six/books/views.py
+++ python-project/books_six/books/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.conf import settings
-from books.models import BookInfo, ImageUpload
+from django.core.paginator import Paginator
+from books.models import BookInfo, ImageUpload, AreasInfo
 
 # 禁止访问的IP列表
 EXCLUDE_IPS = ['192.168.31.157']
@@ -58,6 +59,80 @@ def upload_handle(request):
     return HttpResponse('ok')
 
 
-def show_areas(request):
+# 分页：前端页面应传递需求页面的页码
+def show_areas(request, pageindex):
     """显示区域信息"""
-    return HttpResponse('ok')
+    # 1. 查出所有的省级地区
+    areas = AreasInfo.objects.filter(pid__isnull=True)
+
+    # 2. 分页：创建paginator对象
+    paginator = Paginator(areas, 10)
+    # print(paginator.count)  # 查询集中的总个数
+    # print(paginator.num_pages)  # 分页后的总页数
+    # print(paginator.page_range)  # 分页后的页面页面列表，从1开始
+
+    # 对前端点击传递的页码做判断，默认为第一页
+    if pageindex == '':
+        pageindex = 1
+
+    # 3. 创建Page类对象
+    page = paginator.page(int(pageindex))  # page方法接收页面作为参数，返回对应页的查询集
+    # print(page.number)  # 当前页的页码
+
+    context = {
+        'page': page
+    }
+
+    return render(request, 'books/show_areas.html', context)
+
+
+def areas(request):
+    """省市县选择案例页面"""
+    return render(request, 'books/areas.html')
+
+
+def province(request):
+    """处理ajax请求获取所有省级地区的信息"""
+    provinces = AreasInfo.objects.filter(pid__isnull=True)
+
+    # 对象不能直接转换为json数据格式，需要自己拼接转换:aname, id
+    areas_list = []
+    for area in provinces:
+        areas_list.append((area.id, area.aname))
+
+    # 返回数据
+    context = {
+        'provinces': areas_list
+    }
+
+    return JsonResponse(context)
+
+
+def city(request, pid):
+    """市级地区"""
+    cities = AreasInfo.objects.filter(pid__id=pid)
+
+    areas_list = []
+    for area in cities:
+        areas_list.append((area.id, area.aname))
+
+    context = {
+        'cities': areas_list
+    }
+
+    return JsonResponse(context)
+
+
+def dis(request, cid):
+    """县级地区"""
+    dises = AreasInfo.objects.filter(pid__id=cid)
+
+    areas_list = []
+    for area in dises:
+        areas_list.append((area.id, area.aname))
+
+    context = {
+        'dises': areas_list
+    }
+
+    return JsonResponse(context)
